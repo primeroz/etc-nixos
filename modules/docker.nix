86d43cdcd6ddc4c75b66db9a09907c4c084749f3
Francesco Ciocchetti <primeroznl@gmail.com>
2016-05-17 21:05:25 +0100

    override docker-compose to 1.6.2

--- modules/docker.nix
+++ modules/docker.nix
@@ -6,6 +6,18 @@
    #virtualisation.docker.storageDriver = "zfs";
    virtualisation.docker.extraOptions = "--log-driver journald";
 
+   #packageOverrides = pkgs: rec {
+   #  docker_compose = pkgs.python27Packages.docker_compose.override (
+   #    attrs: {
+   #      version = "1.6.2";
+   #      src = pkgs.fetchurl {
+   #        url = "mirror://pypi/d/docker-compose/${name}.tar.gz";
+   #        sha256 = "10i4032d99hm5nj1p74pcad9i3gz1h5x3096byklncgssfyjqki6";
+   #      };
+   #    };
+   #  );
+   #};
+
    environment.systemPackages = with pkgs; [
         docker
         pkgs.python27Packages.docker_compose
