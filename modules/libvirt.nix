8f4c4c142aa2ba09fb99957ae41ec56ff5cfdea9
Francesco Ciocchetti <primeroznl@gmail.com>
2016-06-17 17:05:56 +0100

    Add KVM support and onshutdown to libvirt

--- modules/libvirt.nix
+++ modules/libvirt.nix
@@ -3,6 +3,8 @@
 
 {
    virtualisation.libvirtd.enable = true;
+   virtualisation.libvirtd.enableKVM = true;
+   virtualisation.libvirtd.onShutdown = "shutdown";
 
    #environment.systemPackages = with pkgs; [
    #     docker
