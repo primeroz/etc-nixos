43a4b5a0a6654fafad4716bf4a4fb69c3f8d7d2d
Francesco Ciocchetti <primeroznl@gmail.com>
2016-07-27 20:36:15 +0100

    add przwork machine and work-laptop profile

---
+++ profiles/work-laptop.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ../modules/base.nix
+    #../modules/thinkpad.nix
+    ../modules/useroptions.nix
+    ../modules/user.nix
+    ../modules/yubikey.nix
+    ../modules/ledger.nix
+    #../modules/docker.nix
+    ../modules/x11.nix
+    ../modules/x11
+    #../modules/libvirt.nix
+  ];
+}
